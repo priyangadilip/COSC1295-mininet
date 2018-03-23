6c23fa82a52359a2486726e545b1c1f3063da236
priyangadilip <priyangadilip@gmail.com>
2018-03-23 16:25:00 +0100

    Add driver.java basic structure, add options enum

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,19 @@
 package mininet;
 
+import java.util.*;
+
 public class Driver {
+	private static Scanner reader = new Scanner(System.in);
+	
+	private static final String MENU_OPTIONS = "\n MiniNet Menu \n================================= \n"
+			+ "1. List everyone \n"
+			+ "2. Add person \n"
+			+ "3. Select person \n"
+			+ "4. Update person \n"
+			+ "5. Connect Persons \n"
+			+ "6. Is direct friend \n"
+			+ "7. Delete person \n"
+			+ "8. Quit \n\n"
+			+ "Enter an option :: ";
+	
 }
---
+++ src/Options.java
@@ -0,0 +1,15 @@
+package mininet;
+
+import java.util.*;
+
+enum Options {
+	LIST_EVERYONE,
+	ADD_PERSON,
+	SELECT_PERSON,
+	UPDATE_PERSON,
+	CONNECT_PERSONS,
+	IS_DIRECT_FRIEND,
+	DELETE_PERSON,
+	QUIT
+};
+
