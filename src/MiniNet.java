e3739a1e8d6d701d8d7c95d62c6a6d8a75fa7f69
ksuraj93 <mahi.suraj93@gmail.com>
2018-03-24 22:00:00 +0100

    Add mininet java

--- src/MiniNet.java
+++ src/MiniNet.java
@@ -1,5 +1,20 @@
 package mininet;
 
 public class MiniNet {
+	private static final boolean ENV_DEVELOPMENT = true;
 
+	public static void main(String args[]) {
+		Driver menuDriver = new Driver();
+		
+		try {
+		  menuDriver.startMenu();
+		} catch (Exception e) {
+			System.out.println("There was some error. Please try again ");
+			
+			if(ENV_DEVELOPMENT) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 }
