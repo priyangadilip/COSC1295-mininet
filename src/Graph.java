31532ecd371690b009e4cbb4bfb018f95cab96f6
ksuraj93 <mahi.suraj93@gmail.com>
2018-03-25 12:00:00 +0100

    Add graph.java to maintain network

---
+++ src/Graph.java
@@ -0,0 +1,20 @@
+package mininet;
+
+public class Graph {
+  private HashMap<Integer, Connection> network = new HashMap<Integer, Connection>();
+
+  // Vertex
+  public void addPerson(Person person) {
+  }
+
+  // Edges
+  public void connectPersons(String personAName, String personBName, Relation relation) {
+  }
+
+  public void deletePersonByName(String name) {
+  }
+  
+  // Change it - Temp
+  public void displayAll() {
+  }
+}
