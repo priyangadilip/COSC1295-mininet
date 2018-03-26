bdda02e273c49aa7f0ebcee398ea0913c7635a0e
ksuraj93 <mahi.suraj93@gmail.com>
2018-03-26 19:05:00 +0100

    Add isDirectFriend and linkWithPerson methods to connection.java

--- src/Connection.java
+++ src/Connection.java
@@ -13,4 +13,18 @@ public class Connection {
 	public Person getPerson() {
 		return person;
 	}
+	
+	public void linkWithPerson(Person p, Relation relation) {
+		links.put(p.getName().hashCode(), relation);
+	}
+	
+	public boolean isDirectFriend(Integer hashcode) {
+		Relation r =  links.get(hashcode);
+		
+		if(r == null) {
+			return false;
+		}
+		
+		return true;
+	}
 }
